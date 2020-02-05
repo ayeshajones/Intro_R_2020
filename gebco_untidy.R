4ef4445c7ea1d409fcfad69a74487a83dd7d0b17
ayeshajones <3708711@myuwc.ac.za>
2020-02-05 18:35:00 +0200

    updated

--- gebco_untidy.R
+++ gebco_untidy.R
@@ -1,3 +1,8 @@
+# Day 4 exercise
+# recreate SA's elevation map
+# 5 February 2020
+# Ayesha Jones
+
 # Load packages
 library(tidyverse)
 library(SDMTools)
@@ -8,22 +13,24 @@ library(raster)
 
 #Untidy data
 
-untidy_data <- read.asc("data/gebco_sa.asc")
+adven_data <- read.asc("data/gebco_sa.asc")
 
-untidy_2 <- raster(untidy_data) # converts to raster
-try1 <- as(untidy_2, "SpatialPixelsDataFrame") # force into spatial etc format using "as"
-tidied <- as.data.frame(try1) # save as df
+untidy_adven <- raster(adven_data) # converts to raster & rename
+tidy_adven <- as(untidy_adven, "SpatialPixelsDataFrame") # force into SpatialPixelsDataFrame format using "as" i.e. tidies data
+tidied_final <- as.data.frame(tidy_adven) # save as df
 
+head(tidied_final) # view first 6 rows
+tail(tidied_final) # view last 6 rows
 
 # Final adventure
-tidy_map <- ggplot(data = tidied, aes(x = x, y = y)) +
+tidy_map <- ggplot(data = tidied_final, aes(x = x, y = y)) +
   geom_raster(aes(fill = layer)) +
   scale_fill_gradientn("Elevation/ \nDepth (m)", values = scales::rescale(c(-6129, 0, 1, 3374)), colors = c("black", "darkblue", "slateblue", "darkturquoise", "gray87", "grey78", "chartreuse4", "seagreen", "darkgreen",  "lightgoldenrod", "darkorange3")) +
-  labs(x = "East", y = "South") +
+  labs(x = "", y = "") +
   scale_x_continuous(breaks = seq(10, 35, 5),
                      labels = c("10", "15", "20", "25", "30", "35"),
                      position = "bottom", expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0), name = "South") +
+  scale_y_continuous(expand = c(0, 0)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank())
 tidy_map 
